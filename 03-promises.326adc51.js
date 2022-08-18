4c50acc41208c0308ed00d118f68b9ab7c92997d
Fluebubble <Trots.anatoly@gmail.com>
2022-08-18 03:54:20 +0000

    Deploying to gh-pages from @ Fluebubble/goit-js-hw-09@485a306cf301f2dbcb2fca53b886642d8699d1da 🚀

---
+++ 03-promises.326adc51.js
@@ -0,0 +1,2 @@
+[document.querySelector(".form"),document.querySelector('[name="delay"]'),document.querySelector('[name="step"]'),document.querySelector('[name="amount"]'),document.querySelector('[type="submit"]')][0].addEventListener("submit",(function(e){e.preventDefault(),console.log("GO!")}));
+//# sourceMappingURL=03-promises.326adc51.js.map
