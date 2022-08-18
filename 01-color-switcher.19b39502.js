cf45fdf2f1725b6a9988428d6cc27198109323a5
Fluebubble <Trots.anatoly@gmail.com>
2022-08-18 23:55:43 +0000

    Deploying to gh-pages from @ Fluebubble/goit-js-hw-09@9bc3aa6dc7a02074a0fffa2099627016d1919cff 🚀

--- 01-color-switcher.19b39502.js
+++
@@ -1,2 +0,0 @@
-const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.stopBtn.disabled=!0;let e=0;t.startBtn.addEventListener("click",(()=>{t.startBtn.disabled=!0,t.stopBtn.disabled=!1,e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stopBtn.addEventListener("click",(()=>{clearInterval(e),t.startBtn.disabled=!1,t.stopBtn.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.19b39502.js.map
